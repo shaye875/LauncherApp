64f130a9e22ceaa2c783f960987058f95536a716
Your Name <you@example.com>
2026-03-16 10:30:21 +0200

    add data connection

---
+++ server/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+
+
+app.listen(3000,()=>{
+    console.log("server run");
+})
+
---
+++ server/data/launching.js
@@ -0,0 +1,11 @@
+import { MongoClient } from 'mongodb'
+import 'dotenv/config'
+
+export async function getCollectin(){
+    const client = new MongoClient(process.env.URL)
+    client.connect()
+    const db = client.db(process.env.DATABASE)
+    const collection = db.collection("launching")
+    return collection
+}
+
